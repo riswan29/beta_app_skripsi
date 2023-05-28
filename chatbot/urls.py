6b6cc894b498de212443ed2dd4c6b680229c0913
riswan29 <wandra290401@gmmail.com>
2023-05-28 23:11:09 +0700

    buat costum layout chat and buat function chatbot history

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -5,4 +5,6 @@ from .views import *
 
 urlpatterns = [
     path('chatbot/', homeBot, name='chatbot'),
+    path('new-chat/', newChat, name='newChat'),
+    path('chat/<str:search>/', loadChat, name='loadChat'),
 ]
