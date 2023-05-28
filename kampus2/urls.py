370f452f336204e922497079e1ec7a4871dd9323
riswan29 <wandra290401@gmmail.com>
2023-05-28 21:39:25 +0700

    add chatbot app to this project and create function to seve user input and responese from bot to excel file

--- kampus2/urls.py
+++ kampus2/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
 
 from app.views import *
 
@@ -15,7 +16,9 @@ urlpatterns = [
     # profile
     path('dosen/profile', dosenProfile, name='profile_dsn'),
     path('dosen/edit', edit_dosen_profile, name='edit-dosen'),
-
     path('mahasiswa/profile', mahasiswaProfile, name='profile_mhs'),
     path('mahasiswa/edit', edit_mahasiswa_profile, name='edit-mahasiswa'),
+    # chatbot
+    path('', include("chatbot.urls")),
+
 ]+ static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
