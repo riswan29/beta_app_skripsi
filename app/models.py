9980d203accc2d02ed6cf396ee2ca5cd92a7e8cb
riswan29 <wandra290401@gmmail.com>
2023-05-28 11:22:27 +0700

    remove db mhs

--- app/models.py
+++ app/models.py
@@ -18,13 +18,3 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
-class Mahasiswa(models.Model):
-    foto_profil = models.ImageField(upload_to='profile_photos/')
-    nama_lengkap = models.CharField(max_length=255)
-    email = models.EmailField()
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='mahasiswa_profile')
-
-    # Tambahkan bidang profil tambahan yang Anda perlukan
-
-    def __str__(self):
-        return self.nama_lengkap
