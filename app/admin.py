5598a8776ffe5cb028e300da7f3249449dac287c
riswan29 <wandra290401@gmail.com>
2023-06-24 09:49:51 +0700

    menambahkan fitur filter ke admin page

--- app/admin.py
+++ app/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'user', 'role', 'nim_nidn')
+    list_filter = ('role',)  # Menambahkan filter berdasarkan role
+    search_fields = ('full_name', 'user__username', 'nim_nidn')  # Menambahkan kolom pencarian
+    ordering = ('full_name',)
+admin.site.register(UserProfile,UserProfileAdmin)
 # admin.site.register(Course)
 admin.site.register(Jadwal)
